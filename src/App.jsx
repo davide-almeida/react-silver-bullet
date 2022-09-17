daddb0ba1248d99d8ab5a98cf667197d88f3392c
Davide Almeida <davide.jhonatan@gmail.com>
2022-09-17 13:43:30 -0300

    fix css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import React from 'react'
+import './index.css'
+
 function App() {
 
   return (
     <div className="text-center">
-      <h1>React-Silver-Bullet - A simple react app with jest configured</h1>
+      <h1>React-Silver-Bullet - A React App with basic Jest settings</h1>
     </div>
   )
 }
